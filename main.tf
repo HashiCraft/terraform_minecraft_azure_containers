a8f3f2a1b049bfd73904683211a59e51933c1c5a
Nicholas Jackson <jackson.nic@gmail.com>
2020-05-11 11:42:44 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -101,12 +101,6 @@ resource "azurerm_container_group" "minecraft" {
   }
 }
 
-resource null_resource "getip" {
-    provisioner "local-exec" {
-    command = "ifconfig"
-  }
-}
-
 output "fqdn" {
   value = azurerm_container_group.minecraft.fqdn
 }
